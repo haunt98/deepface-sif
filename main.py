b8d3bbdcace22a09b7e051e1282e48241d6d841b
haunt98 <hauvipapro@gmail.com>
2026-03-25 03:23:38 +0700

    feat: use lancedb (may change in the future)

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import argparse
 import logging
+from typing import Any
 
 from deepface import DeepFace
+import lancedb
+import pyarrow as pa
 
 MODEL_NAME = "Facenet"
 DETECTOR_BACKEND = "yunet"
-DISTANCE_METRIC = "cosine"
+VECTOR_DIMENSION = 128  # Depends on model
+TABLE_NAME = "face_embeddings"
 
 logger = logging.getLogger(__name__)
 
 
-def run_index(args: argparse.Namespace) -> None:
+def run_index(db: Any, img: str, id: str) -> None:
     try:
         represent_rsp = DeepFace.represent(
-            img_path=args.img,
+            img_path=img,
             model_name=MODEL_NAME,
             detector_backend=DETECTOR_BACKEND,
         )
@@ -31,7 +35,16 @@ def run_index(args: argparse.Namespace) -> None:
         logger.warning("DeepFace.represent return no embedding")
         return
 
-    logger.info(represent_embedding)
+    try:
+        table = db.open_table(TABLE_NAME)
+        row = {
+            "id": id,
+            "vector": represent_embedding,
+        }
+        table.add([row])
+    except Exception:
+        logger.exception("Failed lancedb table add")
+        return
 
 
 def main() -> None:
@@ -46,8 +59,19 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    db = lancedb.connect("deepface.lancedb")
+
+    if TABLE_NAME not in db.table_names():
+        schema = pa.schema(
+            [
+                pa.field("id", pa.string()),
+                pa.field("vector", pa.list_(pa.float32(), VECTOR_DIMENSION)),
+            ]
+        )
+        db.create_table(TABLE_NAME, schema=schema)
+
     if args.command == "index":
-        run_index(args)
+        run_index(db, args.img, args.id)
         return
 
     parser.print_help()
